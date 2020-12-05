16d3608ac14374648ab1f2bf24bb65b5967ce789
AgusTinMd <maidanadiazagustin@gmail.com>
2020-12-05 15:02:36 -0300

    Struct de datos que manejaremos

--- Main_Code.cpp
+++ Main_Code.cpp
@@ -12,6 +12,71 @@ void menudoctor();
 void menuadministracion();
 
 
+struct fecha
+{
+    int dia;
+    int mes;
+    int year;
+};
+
+struct usuario //struct para logueo de usuarios
+{
+  user char[20];
+  pasword char[20];
+};
+
+
+struct asistente //asistente es de lvl 3
+{
+  user char[20];
+  pasword char[20];
+  apynom char[60];
+  int dni;
+  char telefono[25];
+  int lvl;
+};
+
+struct veterinario // veterinario es de lvl 2
+{
+  char user[20];
+  char pasword[20];
+  char apynom[60];
+  int matricula;
+  int dni;
+  char telefono[25];
+  int lvl;
+};
+
+struct administracion //administracion es de lvl 1
+{
+  user char[20];
+  pasword char[20];
+  apynom char[60];
+  int dni;
+  char telefono[25]
+  int lvl;
+};
+
+struct mascota
+{
+  char apynom[60];
+  char domicilio[60];
+  char localidad[60];
+  fecha Nacimiento;
+  int dni_owner; //propietario o dueño es lo mismo
+  int peso;
+  char telefono[25];
+};
+
+
+struct turno
+{
+  int matriculaveterinario;
+  fecha Fecha;
+  int dni_owner;
+  char detalleatencion[380];
+};
+
 int main()
 {
 
@@ -28,7 +93,7 @@ void menulogin()
 prinf("Login");
 printf("---------------------");
 printf("1. Iniciar sesion");
-printf("2. Registrarse");//opcion para la primera vez nada mas
+printf("2. Registrarse"); //opcion para la primera vez nada mas
 printf("3. Salir");
 printf("\n\nIngrese una opcion: ");
 
